b39332980f1f311c56e1ed442f90a1eededd462f
Gabriel Gómez Mera <Gommergab@IESPIRINGALLA.AD>
2023-05-15 19:24:29 +0200

    completado salto

--- Assets/Scripts/PlayerNetwork.cs
+++ Assets/Scripts/PlayerNetwork.cs
@@ -6,34 +6,22 @@ using Unity.Netcode.Components;
 
 public class PlayerNetwork : NetworkBehaviour
 {
-   
+
     private void Update()
     {
         if (!IsOwner) return;
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space)&&CanJump())
         {
-            GetComponent<Rigidbody>().AddForce(new Vector3(0,1,0));
+            GetComponent<Rigidbody>().AddForce(new Vector3(0,300f,0));
+
         }
         this.transform.position += new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * 6f * Time.deltaTime;
 
     }
 
-
-    [ServerRpc]
-    void SubmitPositionRequestServerRpc(Vector3 move)
-    {
-        //actualizamos el servidor
-        transform.position += move * 6f * Time.deltaTime;
-        //le decimos que actualice el cliente
-        // SubmitPositionRequestClientRPC(move);
-
-    }
-    [ClientRpc]
-    void SubmitPositionRequestClientRPC(Vector3 move)
-    {
-        //actualizamos el cliente
-        transform.position += move * 6f * Time.deltaTime;
+    private bool CanJump() {
+        return transform.position.y <= 1f;
     }
 
 }
